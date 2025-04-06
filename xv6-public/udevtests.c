92e78c4797939a1c745e7421d28c0536a03f116b
heimsnik <cmheimvik@gmail.com>
2025-04-06 10:52:44 +0200

    Added test fucntion

--- xv6-public/udevtests.c
+++ xv6-public/udevtests.c
@@ -36,7 +36,7 @@ void testsyscalls(){
 void testsched(){
 	int pids[5];
 	int nicevalues[5] = {0, 10, 20, 30, 39};
-	int workload = 10000000; // Adjust workload as needed
+	int workload = 1000000; // Adjust workload as needed
 	
 	for (int i = 0; i < 5; i++) {
 		pids[i] = fork();
@@ -51,6 +51,8 @@ void testsched(){
 			exit();
 		}
 	}
+	//We have now initiated all child processes, their vruntime should be similar
+	ps(0);
 	
 	for (int i = 0; i < 5; i++) {
 		wait();
