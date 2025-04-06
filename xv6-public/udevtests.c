f20d1fa9f11079a0dcc5ef7d010acd8a69ac4ca1
heimsnik <cmheimvik@gmail.com>
2025-04-06 11:11:31 +0200

    Added test fucntion

--- xv6-public/udevtests.c
+++ xv6-public/udevtests.c
@@ -43,6 +43,13 @@ void testsched(){
 		if (pids[i] == 0) {
 			setnice(getpid(), nicevalues[i]);
 			printf(1, "PID %d | nice %d started.\n", getpid(), nicevalues[i]);
+			if(i == 2) {
+				int j = 0;
+				while(j != 100000){
+					j++;
+				}
+				ps(0);//Pids should be similar here
+			}
 			if(i == 4) {
 				int j = 0;
 				while(j != 10000){
