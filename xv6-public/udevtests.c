7b14a2d955b3b3c69cf3c907285c8309c1319719
heimsnik <cmheimvik@gmail.com>
2025-04-06 10:31:47 +0200

    Added test fucntion

--- xv6-public/udevtests.c
+++ xv6-public/udevtests.c
@@ -42,12 +42,12 @@ void testsched(){
 		pids[i] = fork();
 		if (pids[i] == 0) {
 			setnice(getpid(), nicevalues[i]);
-			printf(1, "PID %d | nice %d started.\n", getpid(), nicevalues[i]);
+			//printf(1, "PID %d | nice %d started.\n", getpid(), nicevalues[i]);
 			volatile int sum = 0;
 			for (volatile int j = 0; j < workload; j++) {
 				sum += j;
 			}
-			printf(1, "PID %d | nice %d finished\n", getpid(), nicevalues[i]);
+			//printf(1, "PID %d | nice %d finished\n", getpid(), nicevalues[i]);
 			exit();
 		}
 	}
