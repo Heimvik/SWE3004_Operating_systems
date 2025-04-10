8957bbdd2febe6f08a922cdb8016c89603fb2526
heimsnik <cmheimvik@gmail.com>
2025-04-10 13:42:07 +0200

    Added printing of exe

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -441,7 +441,7 @@ cfsscheduler(void)
 		switchuvm(p);			//Switch from the schedulers page table to the process's page table
 		p->state = RUNNING;	
     cprintf("%d ",p->pid);
-    if(totalticks % 1e5 == 0){
+    if(totalticks % 100000 == 0){
       cprintf("\n");
     }
 		swtch(&(c->scheduler), p->context);			//Exe appears in and out of this swtch by doing context switching (including stack and instruction pointers)
